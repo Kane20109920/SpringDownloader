0cb0704da56045c2b879f0c2408456c8220166ca
Kane <kane20109920@gmail.com>
2019-01-27 23:08:32 +0800

    add thymeleaf

---
+++ src/main/java/com/kane/SpringDownloader/Controller/TemplateController.java
@@ -0,0 +1,15 @@
+package com.kane.SpringDownloader.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class TemplateController {
+    @RequestMapping("/view")
+    public String imdexTemplate(@RequestParam(value = "name",required = false,defaultValue = "qqq") String name, Model model){
+        model.addAttribute("name",name);
+        return "index2";
+    }
+}
--- src/main/java/com/kane/SpringDownloader/RestController/GetController.java
+++ src/main/java/com/kane/SpringDownloader/RestController/GetController.java
@@ -1,13 +1,17 @@
 package com.kane.SpringDownloader.RestController;
 
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class GetController {
-    @RequestMapping("/456")
+    @RequestMapping("/")
     public String getIndex(){
-        return "123";
+        return "forward:/html/index2.html";
     }
+
+
 
 }
